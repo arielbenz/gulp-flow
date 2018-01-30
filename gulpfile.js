b7f59c91c439527c7bad8ce9918a7fd17e3016b7
Ariel Benz <arielmbenz@gmail.com>
2018-01-30 10:09:20 -0300

    Update Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,35 @@
 
 var namespace = 'gulp-flow';
 
-/* Requires */
 var gulp        = require('gulp'),
     watch       = require('gulp-watch'),
-    // Utilities
     gutil       = require('gulp-util'),
     plumber     = require('gulp-plumber'),
     slang       = require('gulp-slang'),
     debug       = require('gulp-debug'),
     options     = require('gulp-options'),
-    // Styles
     sass        = require('gulp-sass'),
     sourcemaps  = require('gulp-sourcemaps'),
-    // Scripts
     eslint      = require('gulp-eslint');
 
-/* Paths */
 var root        = 'ui.apps/src/main/content/jcr_root/',
     components  = root + 'apps/' + namespace + '/components/',
     designs     = root + 'etc/designs/' + namespace + '/',
     clientlibs  = root + 'etc/clientlibs/' + namespace + '/',
-
-    // Styles
     cssPath     = clientlibs + 'css/',
     sassPath    = clientlibs + 'scss/',
     mainCss     = clientlibs + 'styles/main.scss',
     cssBuild    = cssPath + 'main.css',
     cssSrcMaps  = cssPath + 'main.css.map',
-
-    // Scripts
-    jsPath      = clientlibs + 'js/internal',
-
-    // Images
-    imgPath     = designs + 'images/';
+    jsPath      = clientlibs + 'js/internal';
 
 
 /**
- * Options
+ * Helper: options
+ * Add options parameters in Gulp.
  */
 
-function Options() {
+function options() {
     var opts = {
         prod: false,
         debug: false,
@@ -60,35 +49,22 @@ function Options() {
 }
 
 /**
- * Helper: changeNotification
- * Logs an event to the console.
- *
- * @param {String} fType - The file type that was changed.
- * @param {String} eType - The event that occured.
- * @param {String} msg - Short description of the actions that are being taken.
- */
-
-function changeNotification(fType, eType, msg) {
-    gutil.log(gutil.colors.cyan.bold(fType), 'was', gutil.colors.yellow.bold(eType) + '.', msg);
-}
-
-/**
  * Task: `sass:build`
- * Compiles the sass and writes sourcemaps.
+ * Compiles the scss files.
  */
 gulp.task('sass:build', function (cb) {
     var compressed = 'compressed';
-    if (Options().debug) {
+    if (options().debug) {
         gutil.log('File ' + gutil.colors.cyan.bold(maincss));
         compressed = 'expanded';
     }
 
     gulp.src(mainCss)
-        .pipe(plumber()) // Prevents pipe breaking due to error (for watch task)
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: compressed,
-            omitSourceMapUrl: true, // This is hardcoded in the main.scss due to resource path issues
+            omitSourceMapUrl: true,
             includePaths: [sassPath, components]
         }).on('error', sass.logError))
         .pipe(sourcemaps.write('./', {
@@ -101,7 +77,7 @@ gulp.task('sass:build', function (cb) {
 
 /**
  * Task: `sass:sling`
- * Slings the compiled stylesheet and sourcemaps to AEM.
+ * Slings the compiled CSS and sourcemaps to AEM.
  */
 gulp.task('sass:sling', ['sass:build'], function () {
     return gulp.src([cssBuild, cssSrcMaps])
@@ -116,7 +92,7 @@ gulp.task('sass', ['sass:build', 'sass:sling']);
 
 /**
  * Task: `js:lint`
- * Lints project JS, excluding vendor libs.
+ * Lint the javascript files.
  */
 gulp.task('js:lint', function () {
     return gulp.src([components + '**/*.js', jsPath + '**/*.js'])
@@ -126,44 +102,29 @@ gulp.task('js:lint', function () {
 
 /**
  * Task: `watch`
- * Watches the HTML, Sass, and JS for changes. On a file change,
- * will run builds file-type dependently and sling the new files
- * up to AEM.
+ * Watches the HTML, Sass, and JS for changes.
  */
 gulp.task('watch', function () {
-    // Set up our streams
     var jsWatch = gulp.watch([components + '**/*.js', jsPath + '**/*.js'], ['js:lint']),
         sassWatch = gulp.watch([components + '**/*.scss', mainCss, sassPath + '**/*.scss'], ['sass']),
-        markupWatch = gulp.watch([components + '**/**/*.html', components + '**/**/*.jsp']),
-        imgWatch = gulp.watch([imgPath + '**/*']);
+        markupWatch = gulp.watch([components + '**/**/*.html', components + '**/**/*.jsp']);
 
-    gutil.log('Waiting for changes.');
+    gutil.log('Waiting for changes...');
 
-    // js needs to be linted
+    // Check changes for javascript files
     jsWatch.on('change', function (ev) {
-        changeNotification('JS file', ev.type, 'Linting code & slinging to AEM.');
         return gulp.src(ev.path).pipe(slang(ev.path));
     });
 
-    // Sass needs to get built and slung up
+    // Check changes for SCSS files
     sassWatch.on('change', function (ev) {
-        changeNotification('Sass file', ev.type, 'Running compilation & slinging to AEM.');
         return gulp.src(ev.path).pipe(slang(ev.path));
     });
 
-    // Markup just needs to be slung to AEM
+    // Check changes for HTML files
     markupWatch.on('change', function (ev) {
-        changeNotification('Sightly file', ev.type, 'Slinging file to AEM.');
-        return gulp.src(ev.path).pipe(slang(ev.path));
-    });
-
-    // Images just need to be slung to AEM
-    imgWatch.on('change', function (ev) {
-        changeNotification('Image file', ev.type, 'Slinging file to AEM.');
         return gulp.src(ev.path).pipe(slang(ev.path));
     });
 });
 
 gulp.task('build', ['sass:build', 'js:lint']);
-
-gulp.task('default', ['build']);
